156c553d2dcca865e68245d8e9278b8b023cb790
mohamed <mohamedsayedahmd@gmail.com>
2023-08-19 13:43:13 +0300

    consumer config

--- src/main/java/com/kafkaexample/kafkaexample/KafkaApplication.java
+++ src/main/java/com/kafkaexample/kafkaexample/KafkaApplication.java
@@ -15,7 +15,7 @@ public class KafkaApplication {
 	@Bean
 	CommandLineRunner commandLineRunner(KafkaTemplate<String,String> kafkaTemplate){
 		return args -> {
-			for(int i = 0 ;i<10000;i++){
+			for(int i = 0 ;i<100;i++){
 				kafkaTemplate.send("mohamedcode","hello kafka :)" + i);
 			}
 		};
