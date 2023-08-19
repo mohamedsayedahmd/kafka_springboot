a0ac1bae44e716d9c72806897a6c80c3c0a2aeee
mohamed <mohamedsayedahmd@gmail.com>
2023-08-19 13:18:22 +0300

    kafka template

---
+++ src/main/java/com/kafkaexample/kafkaexample/KafkaApplication.java
@@ -0,0 +1,21 @@
+package com.kafkaexample.kafkaexample;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@SpringBootApplication
+public class KafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaApplication.class, args);
+	}
+	@Bean
+	CommandLineRunner commandLineRunner(KafkaTemplate<String,String> kafkaTemplate){
+		return args -> {
+			kafkaTemplate.send("mohamedcode","hello kafka");
+		};
+	}// kafka template
+}
